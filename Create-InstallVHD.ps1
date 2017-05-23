26cf05edbed5c27c49fe4448cd89aa05228185e6
TurboBasic <off@boun.cr>
2017-05-23 18:47:38 +0300

    Multiple configs

--- Create-InstallVHD.ps1
+++ Create-InstallVHD.ps1
@@ -1,4 +1,37 @@
 #
+param( $index = "Insider_Preview_16199" )
+
+$vhdPath  = ".\dist\install.vhdx"
+$vhdMount = ".\_vhd"
+$installISO = ".\dist\w10_unattended_install.iso"
+
+$config = @{ 
+  "Insider_Preview_16199" = "d:\Downloads\Windows Deployment\Windows10_InsiderPreview_EnterpriseVL_x64_en-us_16193.iso"; 
+  "Enterprise_1703" = "D:\ISOs\w10_enterprise_1703\en_windows_10_enterprise_version_1703_updated_march_2017_x64_dvd_10189290.unattended-initial.iso" 
+}
+
+#$winISO = "d:\Downloads\Windows Deployment\Windows10_InsiderPreview_EnterpriseVL_x64_en-us_16193.iso"
+$winISO = $config.get_item($index)
+$answers = ".\src\answers-" + $index
+
+$Partition = New-VHD -Path $vhdPath -SizeBytes 6GB -Dynamic | Mount-VHD -Passthru | `
+    Initialize-Disk -Passthru | New-Partition -AssignDriveLetter -UseMaximumSize |  `
+    Format-Volume -FileSystem NTFS -Confirm:$false -Force 
+$Destination = $Partition.DriveLetter.Tostring() + ":\"
+7z.exe x -y -o"($Destination)" $winISO
+
+Dism /Capture-Image /ImageFile:.\dist\disk_c.wim /CaptureDir:.\src\disk_c /Name:WindowsConfigs
+cp .\dist\disk_c.wim $Destination
+Dism /Capture-Image /ImageFile:.\dist\disk_d.wim /CaptureDir:.\src\disk_d /Name:SoftwareInstallation
+cp .\dist\disk_d.wim $Destination
+
+cp $answers\BootToAudit.xml "$Destination\Autounattend.xml"
+
+# Create-ISO $Destination  $installISO
+Dismount-VHD –Path ".\dist\install.vhdx"
+
+
+
 Function Create-ISO ($ISOfolder, $ISOfile) {
   #Mount-DiskImage  ISO ...
 
@@ -14,23 +47,3 @@ Function Create-ISO ($ISOfolder, $ISOfile) {
 }
 
 
-$vhdPath  = ".\dist\install.vhdx"
-$vhdMount = ".\_vhd"
-$installISO = ".\dist\w10_unattended_install.iso"
-$winISO = "d:\Downloads\Windows Deployment\Windows10_InsiderPreview_EnterpriseVL_x64_en-us_16193.iso"
-
-$Partition = New-VHD -Path $vhdPath -SizeBytes 6GB -Dynamic | Mount-VHD -Passthru | `
-    Initialize-Disk -Passthru | New-Partition -AssignDriveLetter -UseMaximumSize |  `
-    Format-Volume -FileSystem NTFS -Confirm:$false -Force 
-$Destination = $Partition.DriveLetter.Tostring() + ":\"
-7z.exe x -y -o"($Destination)" $winISO
-
-Dism /Capture-Image /ImageFile:.\dist\disk_c.wim /CaptureDir:.\src\disk_c /Name:WindowsConfigs
-cp .\dist\disk_c.wim $Destination
-Dism /Capture-Image /ImageFile:.\dist\disk_d.wim /CaptureDir:.\src\disk_d /Name:SoftwareInstallation
-cp .\dist\disk_d.wim $Destination
-
-cp .\src\answers\BootToAudit.xml "$Destination\Autounattend.xml"
-
-# Create-ISO $Destination  $installISO
-Dismount-VHD –Path ".\dist\install.vhdx"
