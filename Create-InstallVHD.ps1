04577828e335085324b6825037d0dd67ffe2f60c
TurboBasic <off@boun.cr>
2017-05-24 19:05:14 +0300

    edit aswers files

--- Create-InstallVHD.ps1
+++ Create-InstallVHD.ps1
@@ -1,17 +1,22 @@
 #
-param( $index = "Insider_Preview_16199" )
+param( $index = "Insider_Preview_16193" )
 
 $vhdPath  = ".\dist\install.vhdx"
 $vhdMount = ".\_vhd"
 $installISO = ".\dist\w10_unattended_install.iso"
 
 $config = @{ 
-  "Insider_Preview_16199" = "d:\Downloads\Windows Deployment\Windows10_InsiderPreview_EnterpriseVL_x64_en-us_16193.iso"; 
+  "Insider_Preview_16193" = "d:\Downloads\Windows Deployment\Windows10_InsiderPreview_EnterpriseVL_x64_en-us_16193.iso";  
+  "Insider_Preview_16199" = "D:\ISOs\w10_insider_16199\source.iso"; 
   "Enterprise_1703" = "D:\ISOs\w10_enterprise_1703\en_windows_10_enterprise_version_1703_updated_march_2017_x64_dvd_10189290.unattended-initial.iso" 
 }
 
-#$winISO = "d:\Downloads\Windows Deployment\Windows10_InsiderPreview_EnterpriseVL_x64_en-us_16193.iso"
-$winISO = $config.get_item($index)
+try {
+  $winISO = $config.get_item($index)
+} catch {
+  echo "Configuration $index does not exist"
+  exit
+}
 $answers = ".\src\answers-" + $index
 
 $Partition = New-VHD -Path $vhdPath -SizeBytes 6GB -Dynamic | Mount-VHD -Passthru | `
