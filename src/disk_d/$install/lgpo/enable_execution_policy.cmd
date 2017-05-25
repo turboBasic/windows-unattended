f720ff4fb07f434f4a6a4a35daff7c83880c06a5
TurboBasic <off@boun.cr>
2017-05-25 13:34:23 +0300

    Fix GPO restore script

--- src/disk_d/$install/lgpo/enable_execution_policy.cmd
+++ src/disk_d/$install/lgpo/enable_execution_policy.cmd
@@ -1,6 +1,7 @@
-copy lgpo.exe %windir%\system32
-lgpo.exe /m .\powershell_policies\machine_registry.pol
-lgpo.exe /u .\powershell_policies\user_registry.pol
+SET p=%~dp0
+copy %p%\lgpo.exe %windir%\system32
+lgpo.exe /m %p%\powershell_policies\machine_registry.pol
+lgpo.exe /u %p%\powershell_policies\user_registry.pol
                                                           
 
 
